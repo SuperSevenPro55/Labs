fc1ec8d4e3f13bf3d6ad8a9bb0ce2aafb563212e
Roman Plotskiy <romanplotskiy@gmail.com>
2025-12-04 21:39:20 +0600

    That was used only for test Git

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Labs.*;
 
 public class Main {
     public static void main(String[] args) {
-        Lab_3_2 lab = new Lab_3_2();
-        lab.start();
+        Menu();
+        System.out.println("Завершение программы...");
     }
 }
