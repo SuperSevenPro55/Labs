0da0bd22ac46daabef4dd82144fa89cee65f3b4c
Roman Plotskiy <romanplotskiy@gmail.com>
2025-12-12 00:35:47 +0600

    AI recovery of labs done before

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Labs.*;
 import java.util.Scanner;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Menu();
         System.out.println("Завершение программы...");
     }
@@ -13,19 +13,53 @@ public class Main {
         do {
             System.out.print("""
                 --------Главное меню--------
+                1. База
                 2. Сортировки
                 3. Сортировки 2
                 0. Выход
                 """);
 
             switch (InputRequest()) {
+                case 1:
+                    System.out.print("""
+                        --------База--------
+                        1. Проверка скобок
+                        2. Удалить на месте
+                        0. В главное меню
+                        """);
+                    switch (InputRequest()) {
+                        case 1:
+                            System.out.println("--------Проверка скобок--------");
+                            Lab_1_1.start();
+                            break;
+                        case 2:
+                            System.out.println("--------Удалить на месте--------");
+                            Lab_1_2.start();
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Неверный ввод");
+                    }
+                    break;
+
                 case 2:
                     System.out.print("""
                         --------Сортировки--------
+                        1. Сортировка вставками
+                        2. Сортировка слиянием
                         3. Сортировка структур
                         0. В главное меню
                         """);
                     switch (InputRequest()) {
+                        case 1:
+                            System.out.println("--------Сортировка вставками--------");
+                            Lab_2_1.start();
+                            break;
+                        case 2:
+                            System.out.println("--------Сортировка слиянием--------");
+                            Lab_2_2.start();
+                            break;
                         case 3:
                             System.out.println("--------Сортировка структур--------");
                             Lab_2_3.start();
@@ -40,13 +74,23 @@ public class Main {
                 case 3:
                     System.out.print("""
                         --------Сортировки 2--------
+                        1. Сортировка подсчетом
                         2. Цифровая сортировка
+                        3. Сохранение обобщения
                         0. В главное меню
                         """);
                     switch (InputRequest()) {
+                        case 1:
+                            System.out.println("--------Сортировка подсчетом--------");
+                            Lab_3_1.start();
+                            break;
                         case 2:
                             System.out.println("--------Цифровая сортировка--------");
                             Lab_3_2.start();
+                            break;
+                        case 3:
+                            System.out.println("--------Сохранение обобщения--------");
+                            Lab_3_3.start();
                             break;
                         case 0:
                             break;
@@ -68,7 +112,6 @@ public class Main {
     public static int InputRequest() {
         System.out.print("Выберите пункт: ");
         Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        return choice;
+        return sc.nextInt();
     }
 }
