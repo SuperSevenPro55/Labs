fd3854c1fbcbe8dbbcfeddb4bdfe9b827ae8418e
Roman Plotskiy <romanplotskiy@gmail.com>
2025-12-11 11:12:56 +0600

    Completed the base start() function

--- src/Labs/Lab_3_2.java
+++ src/Labs/Lab_3_2.java
@@ -1,7 +1,32 @@
 package Labs;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import Sorting.sorting;
+import Sorting.RadixSort;
+
 public class Lab_3_2 {
     public static void start() {
-        // TODO
+        List<BigInteger> nums = new ArrayList<>();
+        Input(nums);
+
+        sorting<BigInteger> radix_sort = new RadixSort();
+        radix_sort.sort(nums);
+
+
     }
+
+    public static void Input(List<BigInteger> nums) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите количество элементов: ");
+        int quantity = sc.nextInt();
+        System.out.println("Введите элементы:");
+        for (int i = 0; i < quantity; i++) {
+            nums.add(sc.nextBigInteger());
+        }
+    }
+
+
 }
